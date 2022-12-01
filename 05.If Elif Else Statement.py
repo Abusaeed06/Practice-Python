b96e0afd7d9562c6f3112c6893e7dd0f50f1a47a
Md Abu Saeed <110819004+Abusaeed06@users.noreply.github.com>
2022-12-02 03:03:07 +0600

    Add files via upload

--- 05.If Elif Else Statement.py
+++ 05.If Elif Else Statement.py
@@ -5,10 +5,50 @@
 #Of your output 78.44 degree fahrenheit then submit your code
 
 
-Temp_c=float(input("Enter temp in celsius scale: "))
+#Temp_c=float(input("Enter temp in celsius scale: "))
 
-Tem_f=1.8*Temp_c+32
-print("Temperature in fahrenheit scale is"+str(Tem_f)+"F")
+#Tem_f=1.8*Temp_c+32
+#print("Temperature in fahrenheit scale is"+str(Tem_f)+"F")
 #or
-print("Temperature in fahrenheit scale is",str(Tem_f),"F")
+#print("Temperature in fahrenheit scale is",str(Tem_f),"F")
+
+
+
+### Control flow
+
+Temp=56
+if Temp>30:
+    print("It's Hot out there")
+    print("Dont go out")
+else:
+    print("It's normal out there")
+
+print(3==3)
+print(3!=3)
+print(3<2)
+print(3>2)
+print(3<=2)
+print(3>=2)
+print(3>2 or 3==2)
+print(3>2 and 3==2)
+#1
+#age=16
+# if age>18:
+#     print("Applicanrt is eligible for loan")
+# else:
+#     print("Applicant is not eligible for loan")
+#
+#2
+# age=14
+# if age>=18:
+#     msg="Eligible"
+# else:
+#     msg="not eligible"
+#
+# print(msg)
+
+#3
+age=12
+msg="Eligible"if age>=18 else "not Eligible"
+print(msg)
 
