08dd9018fcb226bf2c422adf7b8bd2994404a39c
Md Abu Saeed <110819004+Abusaeed06@users.noreply.github.com>
2022-12-17 03:49:02 +0600

    Add files via upload

---
+++ 09.While loop.py
@@ -0,0 +1,23 @@
+# x=1
+# while x<11:
+#     print("This is line number",x)
+#     x+=1
+
+x=0
+while x<100:
+    print("This is a line",x)
+    x+=1
+
+
+########INFINITE LOOP#######
+
+y=0
+while y<10:
+    if y==5:
+        break
+    print(y)
+    y += 1
+
+
+
+
